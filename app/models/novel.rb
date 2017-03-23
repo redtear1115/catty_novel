37e9e5307c6da3fdf4e7139ae61fa83c2b1faa1c
Ray Lee <ray-lee@kdanmobile.com>
2017-03-23 13:59:10 +0800

    refactor Crawler service

--- app/models/novel.rb
+++ app/models/novel.rb
@@ -2,32 +2,6 @@ class Novel < ApplicationRecord
   has_many :collections
   has_many :chapters
   
-  def sync
-    target_urls.each do |url|
-      doc = Nokogiri::HTML(open(url))
-      insert_chapter(doc)
-    end
-    self.update(last_sync_url: target_urls.last)
-  end
-  
-  def insert_chapter(doc)
-    postlist = doc.css('#postlist .plhin .t_f')
-    postlist.each do |post|
-      chapter = self.chapters.find_or_create_by(external_id: post['id'])
-      chapter.update(content: post.content)
-    end
-    return self.chapters.count
-  end
-  
-  def target_urls(full=false)
-    return [] if source_page.nil?
-    self.update(last_sync_url: self.source_url) if self.last_sync_url.nil?
-    
-    last_page_num = get_last_page_num(source_page)
-    loop_times = full ? last_page_num : calc_loop_times(last_page_num)
-    calc_urls(loop_times)
-  end
-  
   def chapter_index
     @chapter_index ||= []
     if @chapter_index.empty?
@@ -51,30 +25,5 @@ class Novel < ApplicationRecord
     next_idx = array_idx == (self.chapter_index.count - 1) ? array_idx : array_idx + 1
     return { prev: prev_idx, curr: array_idx, next: next_idx }
   end
-  
-  private 
-  def source_page
-    return nil if self.source_url.nil?
-    @source_page ||= Nokogiri::HTML(open(self.source_url))
-  end
-  
-  def get_last_page_num(source_page)
-    return source_page.css('.pgt .pg .last').first.content.gsub(/\D/,'').to_i
-  end
-  
-  def calc_loop_times(last_page_num)
-    splited_url = self.last_sync_url.split('-')
-    return last_page_num - splited_url[2].to_i + 1
-  end
-  
-  def calc_urls(loop_times)
-    target_urls = []
-    loop_times.times do |index|
-      splited_url = self.last_sync_url.split('-')
-      splited_url[2] = splited_url[2].to_i + index
-      target_urls << splited_url.map(&:to_s).join('-')
-    end
-    return target_urls
-  end
-  
+    
 end
---
+++ app/services/crawler.rb
@@ -0,0 +1,47 @@
+class Crawler
+  
+  def initialize
+  end
+  
+  def sync(novel, full=false)
+    novel.update(last_sync_url: novel.source_url) if novel.last_sync_url.nil?
+    target_urls(novel, full).each { |url| insert_chapter(novel, url) }
+    return novel.chapters.count
+  end
+  
+  def target_urls(novel, full=false)
+    return [] if novel.source_url.nil?
+    source_page = Nokogiri::HTML(open(novel.source_url))
+    return [] if source_page.nil?
+    last_page_num = source_page.css('.pgt .pg .last').first.content.gsub(/\D/,'').to_i
+    loop_times = full ? last_page_num : calc_loop_times(last_page_num, novel.last_sync_url)
+    calc_urls(loop_times, novel.last_sync_url)
+  end
+  
+  private
+  def insert_chapter(novel, url)
+    doc = Nokogiri::HTML(open(url))
+    postlist = doc.css('#postlist .plhin .t_f')
+    postlist.each do |post|
+      chapter = novel.chapters.find_or_create_by(external_id: post['id'])
+      chapter.update(content: post.content)
+    end
+    return  novel.update(last_sync_url: url)
+  end
+  
+  def calc_loop_times(last_page_num, last_sync_url)
+    splited_url = last_sync_url.split('-')
+    return last_page_num - splited_url[2].to_i + 1
+  end
+  
+  def calc_urls(loop_times, last_sync_url)
+    target_urls = []
+    loop_times.times do |index|
+      splited_url = last_sync_url.split('-')
+      splited_url[2] = splited_url[2].to_i + index
+      target_urls << splited_url.map(&:to_s).join('-')
+    end
+    return target_urls
+  end
+  
+end
