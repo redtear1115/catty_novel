96624d61f29a9f2bae87fce0a0b2cea3b8779e0e
Ray Lee <ray-lee@kdanmobile.com>
2017-04-05 17:58:39 +0800

    move to fixed ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '104.199.156.165', user: 'vfgcees', roles: %w{app db web}#, my_property: :my_value
+server '104.199.146.39', user: 'vfgcees', roles: %w{app db web}#, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
