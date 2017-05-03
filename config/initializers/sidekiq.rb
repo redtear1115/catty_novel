a8ef31f1cdcd921e8048fbc5e89d6c1f4d714558
Ray Lee <ray-lee@kdanmobile.com>
2017-05-03 17:39:42 +0800

    setup sidekiq namespace

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,13 @@
-require 'sidekiq'
-
-Sidekiq.configure_client do |config|
-  config.redis = { :size => 1 }
+Sidekiq.configure_server do |config|
+  config.redis = {
+    url: 'redis://127.0.0.1:6379/0',
+    namespace: 'cn'
+  }
 end
 
-Sidekiq.configure_server do |config|
-  config.redis = { :size => 4 }
+Sidekiq.configure_client do |config|
+  config.redis = {
+    url: 'redis://127.0.0.1:6379/0',
+    namespace: 'cn'
+  }
 end
