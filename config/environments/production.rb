caa2d9ebbdad61a75f2ffc5dabad0fdd10d21172
Ray Lee <ray-lee@kdanmobile.com>
2017-05-03 23:40:36 +0800

    try this

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,11 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "catty_novel_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = { 
+    protocol: 'http',
+    host: 'ec2-52-76-158-43.ap-southeast-1.compute.amazonaws.com', 
+    port: 80 
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -79,11 +83,8 @@ Rails.application.configure do
   logger.formatter = config.log_formatter
   config.logger = ActiveSupport::TaggedLogging.new(logger)
   
-  Rails.application.routes.default_url_options = {
-    host: 'http://ec2-52-76-158-43.ap-southeast-1.compute.amazonaws.com',
-    port: 80
-  }
-
+  Rails.application.routes.default_url_options = config.action_mailer.default_url_options
+  config.action_controller.default_url_options = config.action_mailer.default_url_options
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
