210e0fc2efd7da62cf395d3eed810c94fef76f6b
Ray Lee <ray-lee@kdanmobile.com>
2017-05-03 20:57:28 +0800

    csrf token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, prepend: true
-  skip_before_filter :verify_authenticity_token, if: -> { controller_name == 'sessions' && ['create', 'destroy'].include?(action_name) }
+  skip_before_filter :verify_authenticity_token, if: -> { controller_name == 'sessions' && (action_name == 'create' || action_name == 'destroy') }
 end
