41106e28de3721134858944d2d79e4c91691003a
Ray Lee <ray-lee@kdanmobile.com>
2017-05-16 11:19:52 +0800

    skip verify token on login / logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  skip_before_filter :verify_authenticity_token, if: :actions_skipped
+
+  private
+
+  def actions_skipped
+    skipped_map = {
+      'sessions' => ['create', 'destroy']
+    }
+    return false if skipped_map[controller_name].nil?
+    return skipped_map[controller_name].include?(action_name)
+  end
 end
