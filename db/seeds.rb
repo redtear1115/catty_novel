d6fc536d472d3dcae9a971055ec8f8811c6c924b
Ray Lee <ray-lee@kdanmobile.com>
2017-05-31 16:12:38 +0800

    update db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,7 @@ user = User.find_or_initialize_by(email: Secret.admin.account)
 user.password = Secret.admin.password
 user.password_confirmation = Secret.admin.password
 user.save!
+
+sh = SourceHost.find_or_initialize_by(url: 'https://ck101.com/forum-237-1.html')
+sh.name = '卡提諾論壇::小說::長篇小說'
+sh.save!
